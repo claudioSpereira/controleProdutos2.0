32ebff7218cb840f98340eb0d68df7f63732e0c1
claudioSpereira <claudiosp1942@gmail.com>
2017-11-24 22:01:33 -0300

    mais informacoes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
               <div class="div-square">
                    <a href="blank.html" style="text-decoration: none;" >
                       <i class="fa fa-product-hunt fa-5x"></i>
-                      <h4>PRODUCTS</h4>
+                      <h4>ADD PRODUCTS</h4>
                   </a>
               </div>
             </div>
@@ -18,8 +18,8 @@
             <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">
               <div class="div-square">
                    <a href="blank.html" style="text-decoration: none;" >
-                      <i class="fa fa-product-hunt fa-5x"></i>
-                      <h4>ADD PRODUCTS</h4>
+                      <i class="fa fa-list fa-5x"></i>
+                      <h4>LIST PRODUCTS</h4>
                   </a>
               </div>
             </div>
@@ -69,8 +69,35 @@
               </div>
             </div>
 
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">
+              <div class="div-square">
+                   <a href="blank.html" style="text-decoration: none;" >
+                      <i class="fa fa-list fa-5x"></i>
+                      <h5>LIST COLLABORATOR</h5>
+                  </a>
+              </div>
+            </div>
+
             </painel>
         </div>
     </div>
+
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <painel titulo="Control Panel">
+              <div class="col-md-12 col-sm-5 col-md-4">
+
+                <div class="jumbotron">
+                  <h1>Bem vindo, </h1>
+                  <p>Aqui sim, você terá controle dos seus produtos!!!</p>
+                  <p>O sistema foi desenvolvido para ajuda-lo a ter controle
+                  das informações de rotinas do seu dia.</p>
+                  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+                </div>
+
+              </div>
+            </painel>
+          </div>
+        </div>
 </div>
 @endsection
